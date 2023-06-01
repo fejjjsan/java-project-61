e7262684d3bdc452dd723404f19fcffc0da68c53
Artur Lastovskiy <4372wow@gmail.com>
2023-06-01 15:51:45 +0300

    update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,22 +4,29 @@ import java.util.Scanner;
 
 import static hexlet.code.games.Even.playEvenGame;
 import static hexlet.code.games.Calc.playCalcGame;
+import static hexlet.code.games.Gcd.playGcdGame;
+import static hexlet.code.games.Progression.playProgressionGame;
+import static hexlet.code.games.Prime.playPrimeGame;
 
 
-class Engine {
+public class Engine {
     public static void chooseTheGame() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter the game number and press Enter.");
-        System.out.print("\n1 - Greet \n2 - Even \n3 - Calc \n0 - Exit\nYour choice: ");
-        String userAnswer = scanner.next();
+        System.out.print("\n1 - Greet \n2 - Even \n3 - Calc \n4 - GCD ");
+        System.out.print("\n5 - Progression \n6 - Prime \n0 - Exit \nYour choice: ");
 
-        switch (Integer.parseInt(userAnswer)) {
+
+        String gameNum = scanner.next();
+
+
+        switch (Integer.parseInt(gameNum)) {
             case 0:
                 System.out.print("See you soon!");
                 break;
             case 1:
-                Cli.greeting();
+                greeting();
                 break;
             case 2:
                 playEvenGame();
@@ -27,8 +34,29 @@ class Engine {
             case 3:
                 playCalcGame();
                 break;
+            case 4:
+                playGcdGame();
+            case 5:
+                playProgressionGame();
+            case 6:
+                playPrimeGame();
             default:
                 break;
         }
+    }
+
+    public static String greeting() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nWelcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        return userName;
+    }
+
+    public static String provideUserName() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
     }
 }
