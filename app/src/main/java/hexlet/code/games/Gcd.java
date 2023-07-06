2f904d942bdab25e0c3a2cc952214091b7dbf9e7
Artur Lastovskiy <4372wow@gmail.com>
2023-07-06 21:54:12 +0300

    new method getGcd added/ getData method enhanced/ question variable modified

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -8,32 +8,36 @@ public class Gcd implements Game {
         int num2 = (int) (Math.random() * MAX_RND_NUMBER) + 1;
         int biggerNum = Math.max(num1, num2);
         int lowerNum = Math.min(num1, num2);
-        int rightAnswer = 0;
-        int remainder = 0;
+
         String[] data = new String[2];
+        String question = num1 + " " + num2;
 
-        String question = "Question: " + num1 + " " + num2;
-
-        do {
-            if (biggerNum % lowerNum == 0) {
-                rightAnswer = lowerNum;
-                break;
-            }
-            if (biggerNum % lowerNum != 0) {
-                remainder = biggerNum % lowerNum;
-                biggerNum = lowerNum;
-                lowerNum = remainder;
-            }
-        } while (remainder != 0);
+        int gcd = getGcd(biggerNum, lowerNum);
 
         data[0] = question;
-        data[1] = Integer.toString(rightAnswer);
+        data[1] = Integer.toString(gcd);
 
         return data;
     }
 
     public String getTask() {
         return "Find the greatest common divisor of given numbers.";
+    }
+
+    public static int getGcd(int biggerNum, int lowerNum) {
+        int remainder;
+        int rightAnswer = 0;
+        do {
+            if (biggerNum % lowerNum == 0) {
+                rightAnswer = lowerNum;
+                break;
+            } else {
+                remainder = biggerNum % lowerNum;
+                biggerNum = lowerNum;
+                lowerNum = remainder;
+            }
+        } while (remainder != 0);
+        return rightAnswer;
     }
 }
 
